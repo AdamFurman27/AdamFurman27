01899d4daf9e6cac04ddb264c37da86344f0360a
Tonsofunk <adam.furman27@gmail.com>
2017-01-04 15:22:48 -0500

    added form, trying to get rid of parralax in mobile

--- js/project.js
+++ js/project.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
          
     //$("body.content").snapscroll();
 
+
+
     $('.popup-with-form').magnificPopup({
 		type: 'inline',
 		preloader: false,
@@ -22,51 +24,49 @@ $(document).ready(function(){
 
 	$(".typed-bmore").typed({
 	strings: ["Born in Baltimore.", "Programmer.","Rock Climber.","Hiker,", "Scrambler", "Thinker.", "Political", "Community-Minded", "French", "","","", "Born in Baltimore."],
-	typeSpeed: 40
+	typeSpeed: 60
 	});
 
 	$(".typed-mtl").typed({
 	strings: [ "Musician In Montreal.", "Percussionist.", "Electric-Harp", "Drum-Set.", "Vibraphone.", "Hammer-Dulcimer.", "Pianist.", "German","","","","Musician In Montreal."],
-	typeSpeed: 40
+	typeSpeed: 60
 	});
 
 	$(".typed-nyc").typed({
 	strings: [ "Innovator in New York City.", "Digital Specialist", "Content Strategist", "Tech-Enthusiast", "Coder.", "Entrepreneaur", "English", "","","", "Innovator In New York City."],
-	typeSpeed: 40
+	typeSpeed: 60
 	});
 
-	// document.getElementById("last-block").onscroll = function() {typed()};
 
-	// function typed() {
-	//     document.getElementById("bmore").innerHTML = 
-	//     	$(function(){
-	//         	$(".bmore").typed({
-	//             strings: ["Born In Baltimore.", "Rock Climber."],
-	//             typeSpeed: 0
-	//         	});
- //    		});
-	//     ;
-	// }
+   function initial() {
+      $('.nyc-pic').parallax('../img/city2.jpg');
+      $('.blt-pic').parallax('../img/baltimore.jpg');
+      $('.mtl-pic').parallax('../img/Montreal.jpg');
+ }
 
-	// var groups = {};
-	// $('.galleryItem').each(function() {
-	//   var id = parseInt($(this).attr('data-group'), 10);
-	  
-	//   if(!groups[id]) {
-	//     groups[id] = [];
-	//   } 
-	  
-	//   groups[id].push( this );
-	// });
+ $(window).resize(function(){
+	if ($(window).innerWidth() > 768){
+	 	initial();
+	 } else {
+		$('nyc.pic').removeData('data-parallax');
+		$('blt.pic').removeData('parallax');
+		$('mtl.pic').removeData('parallax');
+	 }
+ });
 
-	// $.each(groups, function() {
-	  
-	//   $(this).magnificPopup({
-	//       type: 'image',
-	//       closeOnContentClick: true,
-	//       closeBtnInside: false,
-	//       gallery: { enabled:true }
-	//   })
+ // jQuery(window).resize(function() {
+ //           if ( jQuery(window).width() < 768 ) return false;
+ //           showCover();
+ //        }).resize();
 
-	// });
+// conditional phrases. 
+// create function for initialization. 
+// To disable use a destroy function
+	// below 768 don't initialize the plugin
+	// $(window).innerWidth(768)
+    // $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+	// 1.) When the page loads, if the browser window is wider than a certain amount, initialize plugin 
+	// 2.) if page width changes, if the width is above a certain amount, and the plugin is not initialized, initialize the plugin. 
+	// 3.) If window is smaller than a certain amount, and the width changes, and the plugin is initialized, un initialize the plugin. 
+
 });
