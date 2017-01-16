21c4605b9b5a5c23e40b335d05192bca5d4e709a
Tonsofunk <adam.furman27@gmail.com>
2017-01-16 11:02:07 -0500

    ajax api

--- js/project.js
+++ js/project.js
@@ -40,35 +40,35 @@ $(document).ready(function(){
 
 
 // ISCROLL.JS
-	// (function(){
- //  var ua = navigator.userAgent,
- //    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+	(function(){
+   var ua = navigator.userAgent,
+     isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
 
- //  if (isMobileWebkit) {
- //    $('html').addClass('mobile');
- //  }
+   if (isMobileWebkit) {
+     $('html').addClass('mobile');
+   }
 
- //  $(function(){
- //    var iScrollInstance;
+   $(function(){
+     var iScrollInstance;
 
- //    if (isMobileWebkit) {
- //      iScrollInstance = new iScroll('wrapper');
+     if (isMobileWebkit) {
+       iScrollInstance = new iScroll('wrapper');
 
- //      $('#scroller').stellar({
- //        scrollProperty: 'transform',
- //        positionProperty: 'transform',
- //        horizontalScrolling: false,
- //        verticalOffset: 150
- //      });
- //    } else {
- //      $('.stellar')({
- //        horizontalScrolling: false,
- //        verticalOffset: 150
- //      });
- //    }
- //  	});
+       $('#scroller').stellar({
+         scrollProperty: 'transform',
+         positionProperty: 'transform',
+         horizontalScrolling: false,
+         verticalOffset: 150
+       });
+     } else {
+       $('.stellar')({
+         horizontalScrolling: false,
+         verticalOffset: 150
+       });
+     }
+   	});
 
-	// })();
+})();
 
 	// document.getElementById("last-block").onscroll = function() {typed()};
 
