b1a4aaeb1579cb6807e182bde9fd8078bb24bfd3
Tonsofunk <adam.furman27@gmail.com>
2017-01-16 09:02:09 -0500

    iscroller install

--- js/project.js
+++ js/project.js
@@ -38,12 +38,37 @@ $(document).ready(function(){
 	});
 
 
-	// $(function(){
-	//  	$.stellar({
-	//  		horizontalScrolling: false,
-	//  		verticalOffset: 150
-	//  	});
-	// });
+
+// ISCROLL.JS
+	(function(){
+  var ua = navigator.userAgent,
+    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+
+  if (isMobileWebkit) {
+    $('html').addClass('mobile');
+  }
+
+  $(function(){
+    var iScrollInstance;
+
+    if (isMobileWebkit) {
+      iScrollInstance = new iScroll('wrapper');
+
+      $('#scroller').stellar({
+        scrollProperty: 'transform',
+        positionProperty: 'transform',
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    } else {
+      $('.stellar')({
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    }
+  	});
+
+	})();
 
 	// document.getElementById("last-block").onscroll = function() {typed()};
 
